44b7f230b32beee9c44c5cb2b9088939ec794fa8
William DiFruscio <william@craftandkeen.ca>
2016-11-23 18:33:03 -0500

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+"use strict";
+const quotes = require("./seinfeld");
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+
+app.use('/', express.static(__dirname + '/mainpage'));
+
+app.get('/quotes',(req, res)=>{
+    if(req){
+        res.end(JSON.stringify(quotes));
+    }
+});
+
+app.get('/george',(res,req)=>{
+    if (req){
+        
+    }
+});
+
+app.get('/jerry',(res,req)=>{
+    if (req){
+        
+    }
+});
+
+app.get('/kramer',(res,req)=>{
+    if (req){
+        
+    }
+});
+
+app.get('/elaine',(res,req)=>{
+    if (req){
+        
+    }
+});
+
+app.get('/random',(req, res)=>{
+    if(req){
+        let quoteRandom = Math.floor(Math.random() * quotes.quotes.length);
+        let random = quotes.quotes[quoteRandom];
+        res.end(JSON.stringify(random));
+    }
+});
+
+app.listen(5000, function () {
+  console.log('App listening on port 5000!')
+})
+
