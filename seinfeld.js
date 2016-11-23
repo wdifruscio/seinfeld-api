b41dfe853d5a71f30497baa3fd6bd1a8cf503bdf
Will DiFruscio <wdifruscio@gmail.com>
2016-11-23 18:33:03 -0500

    init

---
+++ seinfeld.js
@@ -0,0 +1,464 @@
+exports.quotes = [
+
+{
+	"quote": "You look like you live with your mother!",
+	"author": "Jerry",
+	"season": "1",
+	"episode": "1",
+	"image": "./Images/1-1.png"
+},
+{
+	"quote": "If you know what happened in the Mets game don't tell me, I taped it. Hello?",
+	"author": "Jerry",
+	"season": "1",
+	"episode": "1",
+	"image": "./Images/1-2.png"
+},
+
+{
+	"quote": "You done with this?",
+	"author": "Kramer",
+	"season": "1",
+	"episode": "1",
+	"image": "./Images/1-3.png"
+},
+
+{
+	"quote": "This dictionary's no good, we need a medical dictionary! When a patient is difficult, you quone him.",
+	"author": "Kramer",
+	"season": "1",
+	"episode": "2",
+	"image": "./Images/2-1.png"
+},
+
+{
+	"quote": "Kramerica Industries!",
+	"author": "Kramer",
+	"season": "1",
+	"episode": "3",
+	"image": "./Images/3-1.png"
+},
+
+{
+	"quote": "You made a man cry? I've never made a man cry. I even kicked a guy in the groin once and he didn't cry.",
+	"author": "Elaine",
+	"season": "1",
+	"episode": "4",
+	"image": "./Images/4-1.png"
+},
+{
+	"quote": "What do you think a hit man would charge..to rub out a couple of cats?",
+	"author": "Elaine",
+	"season": "1",
+	"episode": "5",
+	"image": "./Images/5-1.png"
+},
+{
+	"quote": "You should do it like a band-aid -- one motion, RIGHT OFF!",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "1",
+	"image": "./Images/6-1.png"
+},
+{
+	"quote": "Levels Jerry..levels. --  I'm getting rid of all my furniture. All of it. And I'm going to build these different levels, with steps, and it'll all be carpeted with a lot of pillows. You know, like ancient Egypt.",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "2",
+	"image": "./Images/7-1.png"
+},
+{
+	"quote": "I hate anyone who ever had a pony growing up!",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "2",
+	"image": "./Images/7-2.png"
+},
+{
+	"quote": "I cannot envision any circumstance in which I'll ever have the opportunity to have sex again. How's it gonna happen? I just don't see how it could occur.",
+	"author": "George",
+	"season": "2",
+	"episode": "2",
+	"image": "./Images/7-3.png"
+},
+{
+	"quote": "Can you do me a solid!?",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "3",
+	"image": "./Images/8-1.png"
+},
+{
+	"quote": "We had a funny guy with us in Korea. A tailgunner. They blew his brains out all over the Pacific. ... There's nothing funny about that.",
+	"author": "Alton Benes(Elaines Father)",
+	"season": "2",
+	"episode": "3",
+	"image": "./Images/8-2.png"
+},
+{
+	"quote": "Women don't want to see need. They want a take-charge guy. A colonel, a kaiser, a czar!",
+	"author": "George",
+	"season": "2",
+	"episode": "4",
+	"image": "./Images/9-1.png"
+},
+{
+	"quote": "Please, don't insult me -- no one's a bigger idiot than me.",
+	"author": "George",
+	"season": "2",
+	"episode": "5",
+	"image": "./Images/10-1.png"
+},
+{
+	"quote": "I lie every second of the day. My whole life is a sham.",
+	"author": "George",
+	"season": "2",
+	"episode": "5",	
+	"image": "./Images/10-2.png"
+},
+{
+	"quote": "People don't turn down money, it's what separates us from the animals!",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "5",	
+	"image": "./Images/10-3.png"
+
+},
+{
+	"quote": "So please, a little respect ... for I am Costanza -- Lord of the idiots!",
+	"author": "George",
+	"season": "2",
+	"episode": "5",	
+	"image": "./Images/10-4.png"
+},
+{
+	"quote": "It was the single most damaging experience of my life -- aside from seeing my father naked",
+	"author": "George",
+	"season": "2",
+	"episode": "6",	
+	"image": "./Images/11-1.png"
+},
+{
+	"quote": "I say we 'get' him",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "6",	
+	"image": "./Images/11-2.png"
+
+},
+{
+	"quote": "You're in big trouble son. Burglary, grand larceny, possession of stolen goods.. and uh, uh.. murder!",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "6",	
+	"image": "./Images/11-3.png"
+},
+{
+	"quote": "C'mon what's the big deal... just gonna put a little concrete in the washing machine",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "7",	
+	"image": "./Images/12-1.png"
+},
+{
+	"quote": "Oh yeah I usually sit around naked a couple hours a day. I cook naked, I clean naked, I drive naked, naked, naked, naked!",
+	"author": "Elaine",
+	"season": "2",
+	"episode": "7",	
+	"image": "./Images/12-2.png"
+},
+{
+	"quote": "Women go after doctors like men go after models -- They want someone with knowledge of the body. We just want the body",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "8",	
+	"image": "./Images/13-1.png"
+},
+{
+	"quote": "The really bad thing about the heart is the sex thing. See, you gotta be careful about sex now. You get that heart pumping and suddenly, boom! Next thing you know, you got a hose coming out of your chest attached to a piece of luggage!",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "8",	
+	"image": "./Images/13-2.png"
+},
+{
+	"quote": "Oh yeah? My friend, Bob Saccomanno, he came in here for a hernia operation.. Oh yeah, routine surgery.. now he's sittin' around in a chair by a window going - 'My name is Bob!'",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "8",	
+	"image": "./Images/13-3.png"
+},
+{
+	"quote": "I completely skipped healthy adulthood. I went from having orgasms immediately to taking forever. You could do your taxes in the time it takes me to have an orgasm.",
+	"author": "George",
+	"season": "2",
+	"episode": "8",	
+	"image": "./Images/13-4.png"
+},
+{
+	"quote": " I'm an eggplant! I'm an eggplant! I'm a minstrel man!",
+	"author": "George",
+	"season": "2",
+	"episode": "8",	
+	"image": "./Images/13-5.png"
+},
+{
+	"quote": "Now you listen to me. I want details and I want them right now. I don't have a job, I have no place to go. You're not in the mood? Well you get in the mood!",
+	"author": "George",
+	"season": "2",
+	"episode": "9",	
+	"image": "./Images/14-1.png"
+},
+{
+	"quote": " Look, will you at least let me bring the guy over? He's an amazing man -- He's a Russian immigrant. He escaped the Gulag! He's like the Sakharov of cable guys.. He'll slow down your gas meter.. He sells slugs! -- Jerry. Slugs for the subway!",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "10",	
+	"image": "./Images/15-1.png"
+},
+{
+	"quote": "My name is Bill.. You might be the last person I ever see",
+	"author": "Bill",
+	"season": "2",
+	"episode": "10",	
+	"image": "./Images/15-2.png"
+},
+{
+	"quote": "Yeah I eat the whole apple.. core, stem, seeds -- everything",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "10",	
+	"image": "./Images/15-3.png"
+},
+{
+	"quote": "You ever eat the bark off of a pineapple?",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "10",	
+	"image": "./Images/15-4.png"
+},
+{
+	"quote": "You walk over that table, you pick up an eggroll, you don't say anything, you eat it, say 'thank you very much', wipe your mouth, walk away- I give you 50 bucks.",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "11",	
+	"image": "./Images/16-1.png"
+},
+{
+	"quote": "Movie hotdogs! I'd rather lick the food off the floor!",
+	"author": "Elaine",
+	"season": "2",
+	"episode": "11",	
+	"image": "./Images/16-2.png"
+},
+{
+	"quote": "I don't know him! -- He could be one of those people who walks around the street pricking people with pins!",
+	"author": "Jerry",
+	"season": "2",
+	"episode": "12",	
+	"image": "./Images/17-1.png"
+},
+{
+	"quote": "Well...... its been a week, its up to the cat now.",
+	"author": "Kramer",
+	"season": "2",
+	"episode": "12",	
+	"image": "./Images/17-2.png"
+},
+{
+	"quote": "Three nights ago, a gas main beneath the restaurant exploded, killing five people in my section, including the busboy who replaced me. If I am not fired that night because of you and your thoughtless, stupid, insensitive remarks, it would have been me. You saved my life! ",
+	"author": "Antonio(The Busboy)",
+	"season": "2",
+	"episode": "12",	
+	"image": "./Images/17-3.png"
+},
+{
+	"quote": "Are you crazy?! -- I can't have a man touching me!",
+	"author": "George",
+	"season": "3",
+	"episode": "1",	
+	"image": "./Images/18-1.png"
+},
+{
+	"quote": "You know my feelings on this-- I don't even pay taxes!",
+	"author": "Kramer",
+	"season": "3",
+	"episode": "2",	
+	"image": "./Images/19-1.png"
+},
+{
+	"quote": "Kramer..saw me naked.",
+	"author": "Elaine",
+	"season": "3",
+	"episode": "2",	
+	"image": "./Images/19-2.png"
+},
+
+{
+	"quote": "It became very clear to me sitting out there today that every decision I've made in my entire life has been wrong. My life is the complete opposite of everything I want it to be. Every instinct I have, in every aspect of life, be it something to wear, something to eat - it's all been wrong.", 
+	"author": "George",
+	"image": ""
+},
+{
+
+	"quote": "I'm not a lesbian. I hate men, but I'm not a lesbian",
+	"author": "Elaine",
+	"image": ""
+
+},
+{
+
+	"quote": "Looking at cleavage is like looking at the sun. You don't stare at it. It's too risky. Ya get a sense of it and then you look away.",
+	"author": "Jerry",
+	"image": ""
+
+},
+{
+
+	"quote": "You have the chicken, the hen, and the rooster. The chicken goes with the hen... So who is having sex with the rooster",
+	"author": "Frank Costanza",
+	"image": ""
+
+},
+{
+
+	"quote": "The cat - mrrreeeooowww - is out of the bag!",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "Jerry, just remember, it's not a lie if you believe it.",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "Do you ever get down on your knees and thank God you know me and have access to my dementia",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "We don't know how long this will last. They are a very festive people.",
+	"author": "Elaine",
+	"image": ""
+
+},
+{
+
+	"quote": "You know I always wanted to pretend I was an architect.",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "If you're not gonna be a part of a civil society, then just get in your car and drive on over to the East Side.",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "I'm speechless. I have no speech.",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "I have been performing feats of strength all morning.",
+	"author": "Frank Costanza",
+	"image": ""
+
+},
+
+{
+
+	"quote": "Hi, my name is George, I'm unemployed and I live with my parents.",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "Just remember, when you control the mail, you control... information.",
+	"author": "Newman",
+	"image": ""
+
+},
+{
+
+	"quote": "I don't trust the guy. I think he regifted, then he degifted, and now he's using an upstairs invite as a springboard to a Super bowl sex romp.",
+	"author": "Jerry",
+	"image": ""
+
+},
+{
+
+	"quote": "I don't think I've ever been to an appointment in my life where I wanted the other guy to show up.",
+	"author": "George",
+	"image": ""
+
+},
+{
+
+	"quote": "Boy, a little too much chlorine in that gene pool.",
+	"author": "Jerry",
+	"image": ""
+
+},
+{
+
+	"quote": "Do you have any idea how much time I waste in this apartment?",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "See, this is what the holidays are all about. Three buddies sitting around chewing gum.",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "The carpet sweeper is the biggest scam perpetrated on the American public since One Hour Martinizing.",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "Here's to feeling good all the time.",
+	"author": "Kramer",
+	"image": ""
+
+},
+{
+
+	"quote": "I'll be back. We'll make out.",
+	"author": "David Puddy",
+	"image": ""
+
+},
+{
+
+	"quote": "Ah, look, I? I'm sorry to bother you, but I'm a US postal worker and my mail truck was just ambushed by a band of backwoods mail-hating survivalists.",
+	"author": "Newman",
+	"image": ""
+
+},
+{
+
+	"quote": "You very bad man, Jerry. Very bad man.",
+	"author": "Babu",
+	"image": ""
+
+    }
+];
