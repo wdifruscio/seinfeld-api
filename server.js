8b9f64eb18bac6357ee304b5f4b577dc9089bccd
William\ DiFruscio <william@craftandkeen.ca>
2016-11-23 20:00:21 -0500

    msg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const quotes = require("./seinfeld");
 const express = require('express');
 const app = express();
-const port = process.env.port || 80;
+let port = process.env.port || 8080;
 
 
 app.use('/', express.static(__dirname + '/mainpage'));
@@ -45,7 +45,7 @@ app.get('/random',(req, res)=>{
     }
 });
 
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('App listening on port 8080!')
 })
 
