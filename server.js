9f1b09614e7e79d3a27e048c1965aa4893d5dcf6
William\ DiFruscio <william@craftandkeen.ca>
2016-11-23 20:20:34 -0500

    fix cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use('/', express.static(__dirname + '/mainpage'));
 
